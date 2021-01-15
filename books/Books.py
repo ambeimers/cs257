40cf32d3ffbfbe4d453f9f25544602fab9756b33
Ann Beimers <abeimers@eduroam-165-60.dyn.carleton.edu>
2021-01-15 13:08:44 -0600

    friday changes

--- books/Books.py
+++ books/Books.py
@@ -1,5 +1,6 @@
 # Chloe Morscheck and Ann Beimers, CS 257
-# Books.py
+# books.py
+# This program sorts a file by parameters from the command-line.
 
 import argparse
 import csv
@@ -7,50 +8,46 @@ import csv
 book_list = []
 
 def get_parsed_arguments():
+#Analyzes command-line arguments to provide search parameters, and returns a namespace object.
+
 	parser = argparse.ArgumentParser(description = 'Sorts books by provided parameters.')
 
-	#file name
 	parser.add_argument('file')
-
-	# --author or -a
 	parser.add_argument('--author', '-a', default='', nargs='*')
-
-	# --title or -t
 	parser.add_argument('--title', '-t', default='', nargs='*')
-
-	# --year or -y
 	parser.add_argument('--year', '-y', default='0 2021', nargs='*')
-
-	# --version or -v
-	parser.add_argument('--version', '-v', action='version', version = '%(prog)s 1.0 by Chloe and Ann, last updated January 15th, 2021')
+	parser.add_argument('--version', '-v', action='version', version = '%(prog)s 1.0, Chloe Morscheck and Ann Beimers, CS 257, January 15, 2021')
 
 	return parser.parse_args()
 
-# Currently prints and inclusive list based on filters, but also prints some things twice. Do we want inclusive? How do we get rid of duplicates?
 def sort_by_author(sorted_list, author):
 	for i in range(len(book_list)-1):
 		if author.lower() in book_list[i][2].lower():
-			sorted_list.append(book_list[i])
+			if sorted_list.count(book_list[i]) == 0:
+				sorted_list.append(book_list[i])
 	return sorted_list
 
 def sort_by_title(sorted_list, title):
 	for i in range(len(book_list)-1):
 		if title.lower() in book_list[i][0].lower():
-			sorted_list.append(book_list[i])
+			if sorted_list.count(book_list[i]) == 0:
+				sorted_list.append(book_list[i])
 	return sorted_list
 
 def sort_by_year(sorted_list, year):
 	if year == []:
 		year = ['0', '2021']
-		# Not ideal to have this hardcoded
+		# Default case: not ideal to have this hardcoded
 	if len(year) == 2:
 		for i in range(len(book_list)-1):
 			if book_list[i][1] >= year[0] and book_list[i][1] <= year[1]:
-				sorted_list.append(book_list[i])
+				if sorted_list.count(book_list[i]) == 0:
+					sorted_list.append(book_list[i])
 	else:
 		for i in range(len(book_list)-1):
 			if book_list[i][1] == year[0]:
-				sorted_list.append(book_list[i])
+				if sorted_list.count(book_list[i]) == 0:
+					sorted_list.append(book_list[i])
 	return sorted_list
 
 def main():
@@ -69,7 +66,6 @@ def main():
 		sorted_list = sort_by_title(sorted_list, title)
 	if arguments.year != '0 2021':
 		sorted_list = sort_by_year(sorted_list, arguments.year)
-
 
 	for i in range(len(sorted_list)):
 		print(sorted_list[i][0] + ", " + sorted_list[i][1] + ", " + sorted_list[i][2])

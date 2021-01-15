d4333d11db14face26aa5b63e7e4ed65489e66be
Ann Beimers <abeimers@eduroam-165-60.dyn.carleton.edu>
2021-01-15 13:21:18 -0600

    changes after office hours

--- books/Books.py
+++ books/Books.py
@@ -5,7 +5,6 @@
 import argparse
 import csv
 
-book_list = []
 
 def get_parsed_arguments():
 #Analyzes command-line arguments to provide search parameters, and returns a namespace object.
@@ -15,20 +14,25 @@ def get_parsed_arguments():
 	parser.add_argument('file')
 	parser.add_argument('--author', '-a', default='', nargs='*')
 	parser.add_argument('--title', '-t', default='', nargs='*')
-	parser.add_argument('--year', '-y', default='0 2021', nargs='*')
+	parser.add_argument('--year', '-y', nargs='*')
 	parser.add_argument('--version', '-v', action='version', version = '%(prog)s 1.0, Chloe Morscheck and Ann Beimers, CS 257, January 15, 2021')
 
 	return parser.parse_args()
 
 def sort_by_author(sorted_list, author):
-	for i in range(len(book_list)-1):
+	#change name (not sorting - filtering?)
+	#option 1: remove stuff that doesn't match from sorted_list
+	#new_list = []
+	#for book in sorted_list:
+	#if book matches, append to new_list and return new_list
+	for i in range(len(book_list)):
 		if author.lower() in book_list[i][2].lower():
 			if sorted_list.count(book_list[i]) == 0:
 				sorted_list.append(book_list[i])
 	return sorted_list
 
 def sort_by_title(sorted_list, title):
-	for i in range(len(book_list)-1):
+	for i in range(len(book_list)):
 		if title.lower() in book_list[i][0].lower():
 			if sorted_list.count(book_list[i]) == 0:
 				sorted_list.append(book_list[i])
@@ -39,12 +43,12 @@ def sort_by_year(sorted_list, year):
 		year = ['0', '2021']
 		# Default case: not ideal to have this hardcoded
 	if len(year) == 2:
-		for i in range(len(book_list)-1):
+		for i in range(len(book_list)):
 			if book_list[i][1] >= year[0] and book_list[i][1] <= year[1]:
 				if sorted_list.count(book_list[i]) == 0:
 					sorted_list.append(book_list[i])
 	else:
-		for i in range(len(book_list)-1):
+		for i in range(len(book_list)):
 			if book_list[i][1] == year[0]:
 				if sorted_list.count(book_list[i]) == 0:
 					sorted_list.append(book_list[i])
@@ -54,14 +58,16 @@ def main():
 	arguments = get_parsed_arguments()
 	author = ' '.join(arguments.author)
 	title = ' '.join(arguments.title)
-	sorted_list = []
+	book_list = []
 	with open('books.csv', newline='') as csvfile:
 		reader = csv.reader(csvfile)
 		for row in reader:
 			book_list.append([row[0],row[1],row[2]])
 
+	#filtered_list = book_list
+
 	if author != '':
-		sorted_list = sort_by_author(sorted_list, author)
+		filtered_list = filtered_by_author(book_list, author)
 	if title != '':
 		sorted_list = sort_by_title(sorted_list, title)
 	if arguments.year != '0 2021':

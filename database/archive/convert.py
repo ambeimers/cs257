814bfcb9eed91539570224bc5114396d417f0f8f
Quoc Nguyen <nguyenq2@carleton.edu>
2021-01-26 19:01:37 -0600

    did stuff

--- database/archive/convert.py
+++ database/archive/convert.py
@@ -4,6 +4,15 @@ Authors: Ann Beimers and Quoc
 
 import csv
 
+
+def remove_commas(input_string):
+  for i in range(len(input_string)):
+    if i > len(input_string)-1:
+      break
+    if input_string[i] == ',':
+      input_string = input_string[0:i] + input_string[i+1:]
+  return input_string
+
 athletes = open('athletes.csv', 'w')
 regions = open('regions.csv', 'w')
 games = open('games.csv', 'w')
@@ -12,17 +21,14 @@ events = open('events.csv', 'w')
 with open('athlete_events.csv') as f:
   csv_reader = csv.reader(f)
   athlete_id = -1
+  row_count = 1
   for row in csv_reader:
     if row[0] == athlete_id:
       continue
     try:
       athlete_id = row[0]
       name = row[1]
-      for i in range(len(name)):
-        if i > len(name)-1:
-          break
-        if name[i] == ',':
-          name = name[0:i]+name[i+1:]
+      name = remove_commas(name)
       sex = row[2]
       age = int(row[3])
       height_centimeters = int(row[4])
@@ -35,18 +41,24 @@ with open('athlete_events.csv') as f:
       city = row[11]
       sport = row[12]
       event = row[13]
+      event = remove_commas(event)
       medal = row[14]
-      print(f"{athlete_id}, {name}, {sex}, {team}", file=athletes)
-      print(f"{game}, {year}, {season}, {city}", file=games)
-      print(f"{event}, {sport}", file=events)
+      print(f"{row_count},{name},{sex},{team},{medal}", file=athletes)
+      print(f"{row_count},{game},{year},{season},{city}", file=games)
+      print(f"{row_count},{event},{sport}", file=events)
+      row_count += 1
     except ValueError:
       continue
 
 
 with open('noc_regions.csv') as f:
   csv_reader = csv.reader(f)
+  row_number = 1
   for row in csv_reader:
     noc = row[0]
     region = row[1]
+    region = remove_commas(region)
     notes = row[2]
-    print(f"{noc}, {region}, {notes}", file=regions)
+    print(f"{row_number},{noc},{region},{notes},{medal}", file=regions)
+    row_number += 1
+

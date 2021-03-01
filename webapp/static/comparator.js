479263209450fc47bddffa70a6ebfd033cb3577e
matthewse19 <mcse@maine.rr.com>
2021-03-01 10:34:35 -0600

    worked on the bar js

--- webapp/static/comparator.js
+++ webapp/static/comparator.js
@@ -3,40 +3,131 @@
 window.onload = initialize;
 
 function initialize() {
-    var element = document.getElementById('search');
-    if (element) {
-        element.onclick = onSearchButton;
+    var selection = document.getElementById("options");
+    selection.addEventListener("change", onOptionChange);
+}
+
+//Changed the text for the input areas according to what they are searching
+function onOptionChange(){
+    var selection = document.getElementById("options");
+    var choice = selection.value;
+
+    var option1 = document.getElementById("option1_input");
+    var option2 = document.getElementById("option2_input");
+    var option1label = document.getElementById("option1_label");
+    var option2label = document.getElementById("option2_label");
+
+    option1label.innerHTML = choice + " 1"
+    option2label.innerHTML = choice + " 2"
+
+    if (choice == "artist"){
+        option1.placeholder = "enter an artist"
+        option2.placeholder = "enter an artist"
+    }else{
+        option1.placeholder = "enter a " + choice
+        option2.placeholder = "enter a " + choice
     }
+
+    if (choice == "year"){
+        assignYearSearch();
+    }
+}
+
+//Generate data list and add it
+function assignYearSearch(){
+    var datalistBody = "";
+    for (var y = 1920; y <= 2021; y++){
+        datalistBody += "<option>" + y + "</option>\n";
+    }
+    var year1Datalist = document.getElementById("year1");
+    var year2Datalist = document.getElementById("year2");
+
+    year1Datalist.innerHTML = datalistBody;
+    year2Datalist.innerHTML = datalistBody;
+    document.getElementById("go").addEventListener("click", yearQuery)
+}
+
+function yearQuery(){
+    var year1 = document.getElementById("option1_input").value;
+    var year2 = document.getElementById("option2_input").value;
+    var allAttributes = ['acousticness', 'danceability', 'duration', 'energy', 'loudness', 'speechiness', 'tempo', 'valence', 'popularity']
+
+    //Assign the button actions and get the extreme songs
+    var url1 = getAPIBaseURL() + '/songs/year/' + year1;
+    var url2 = getAPIBaseURL() + '/songs/year/' + year2;
+    var songs1 = {};
+    var songs2 = {};
+    Promise.all([
+        fetch(url1, {method: 'get'}),
+        fetch(url2, {method: 'get'})
+    ]).then(function (responses) {
+    	// Get a JSON object from each of the responses
+    	return Promise.all(responses.map(function (response) {
+    		return response.json();
+    	}));
+    }).then(function (data) {
+        songs1 = data[0]
+        songs2 = data[1]
+        console.log(songs1);
+        console.log(songs2);
+        for(var i = 0; i < allAttributes.length; i ++){
+            var leftBar = document.getElementById(allAttributes[i]).firstElementChild.firstElementChild;
+            var rightBar = document.getElementById(allAttributes[i]).lastElementChild.firstElementChild;
+            let leftSong = songs1[allAttributes[i]];
+            let rightSong = songs2[allAttributes[i]];
+            leftBar.addEventListener("click", function(){onBarClick(leftSong, true)});
+            rightBar.addEventListener("click", function(){onBarClick(rightSong, false)});
+        }
+
+    }).catch(function (error) {
+    	// if there's an error, log it
+    	console.log(error);
+    });
+
+    //Assign correct length of buttons
+    url1 = getAPIBaseURL() + '/year/' + year1;
+    url2 = getAPIBaseURL() + '/year/' + year2;
+    Promise.all([
+        fetch(url1, {method: 'get'}),
+        fetch(url2, {method: 'get'})
+    ]).then(function (responses) {
+    	// Get a JSON object from each of the responses
+    	return Promise.all(responses.map(function (response) {
+    		return response.json();
+    	}));
+    }).then(function (data) {
+        attributes1 = data[0]
+        attributes2 = data[1]
+
+        for(var i = 0; i < allAttributes.length; i ++){
+            console.log("doing stuff");
+            var leftBar = document.getElementById(allAttributes[i]).firstElementChild.firstElementChild;
+            var rightBar = document.getElementById(allAttributes[i]).lastElementChild.firstElementChild;
+            var value1 = attributes1[allAttributes[i]];
+            var value2 = attributes2[allAttributes[i]];
+            var maxValue = Math.max(value1, value2) * Math.max(value1, value2);
+            leftBar.style.width = ((value1 * value1 / maxValue) * 100) + "%";
+            rightBar.style.width = ((value2 * value2 / maxValue) * 100) + "%";
+        }
+    }).catch(function (error) {
+    	// if there's an error, log it
+    	console.log(error);
+    });
+
+}
+
+function onBarClick(songInfo, onLeft){
+    var webplayer;
+    if(onLeft){
+        webplayer = document.getElementById("left-webplayer").firstElementChild;
+    }else{
+        webplayer = document.getElementById("right-webplayer").firstElementChild;
+    }
+    webplayer.src = "https://open.spotify.com/embed/track/" + songInfo.spotify_id;
+
 }
 
 function getAPIBaseURL() {
     var baseURL = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/api';
     return baseURL;
-}
-
-function onSearchButton() {
-    var year_input = document.getElementById("search_input").value
-    var url = getAPIBaseURL() + '/year/' + year_input;
-    fetch(url, {method: 'get'})
-
-    .then((response) => response.json())
-
-    .then(function(attributes) {
-        var listBody = '';
-        listBody +=
-				'<li>' + 'Year: ' + attributes["year"] + '</li>\n'
-				+ '<li>' + 'Acousticness: ' + attributes["acousticness"] + '</li>\n'
-				+ '<li>' + 'Danceability: ' + attributes["danceability"] + '</li>\n'
-				+ '<li>' + 'Duration (ms): ' + attributes["duration"] + '</li>\n'
-				+ '<li>' + 'Energy: ' + attributes["energy"] + '</li>\n';
-
-        var attributeListElement = document.getElementById('attribute_list');
-        if (attributeListElement) {
-            attributeListElement.innerHTML = listBody;
-        }
-    })
-
-    .catch(function(error) {
-        console.log(error);
-    });
 }
